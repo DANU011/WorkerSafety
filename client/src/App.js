3f2c20390e0fd6aceb9f368a1f11ffac82020493
Erica-Ku <yeskuminji@naver.com>
2023-05-01 17:29:13 +0900

    로그인 후 화면 이동

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
   );
 }
 
