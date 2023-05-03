a6f59692dbc0b7aa4f77401c95d5dacdaf56abed
DANU011 <122770856+DANU011@users.noreply.github.com>
2023-05-03 16:51:27 +0900

    05030451

--- DA/demo/src/main/java/com/example/demo/feign/FlaskClient.java
+++ DA/demo/src/main/java/com/example/demo/feign/FlaskClient.java
@@ -1,4 +1,14 @@
 package com.example.demo.feign;
 
-public class FlaskClient {
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "flask", url = "http://localhost:5000")
+public interface FlaskClient {
+
+    @GetMapping("/hello")
+    String sayHello(@RequestParam("name") String name);
+
 }
+
