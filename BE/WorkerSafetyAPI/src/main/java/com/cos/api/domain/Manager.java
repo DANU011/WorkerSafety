e59d9cf8330ddf53be0876d61a6d55411d1b66ce
Erica-Ku <yeskuminji@naver.com>
2023-06-30 10:51:26 +0900

    오류 수정(관리자 등록)

--- BE/WorkerSafetyAPI/src/main/java/com/cos/api/domain/Manager.java
+++ BE/WorkerSafetyAPI/src/main/java/com/cos/api/domain/Manager.java
@@ -8,7 +8,6 @@ import javax.persistence.Id;
 @Entity
 public class Manager {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private String managerid;
 	private String password;
 	private String name;
